f768fd6137f8eeba50aabf3cea2bac3110dd5be3
wangqisong <wangqisong@live800.com>
2020-06-23 17:27:56 +0800

    redisson测试代码

--- redisson/src/main/java/com.wqs.redisson/config/RedissonConfig.java
+++ redisson/src/main/java/com.wqs.redisson/config/RedissonConfig.java
@@ -1,16 +1,34 @@
 package com.wqs.redisson.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Autowired
+    Config config;
 
     @Bean
     public Config config(){
         Config config = new Config();
         config.useSingleServer().setAddress("redis://172.18.18.160:6379").setPassword("123456");
         return config;
+    }
+
+    @Bean
+    public RedissonClient client(){
+        RedissonClient client = Redisson.create(config);
+        return client;
+    }
+
+    @Bean("rc")
+    public RedissonReactiveClient reactiveClient(){
+        RedissonReactiveClient reactiveClient = Redisson.createReactive(config);
+        return reactiveClient;
     }
 }
--- redisson/src/test/java/com/wqs/redisson/test/RedissonApplicationTest.java
+++ redisson/src/test/java/com/wqs/redisson/test/RedissonApplicationTest.java
@@ -1,15 +1,17 @@
 package com.wqs.redisson.test;
 
 import com.wqs.redisson.RedissonApplication;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.Redisson;
-import org.redisson.api.RKeys;
-import org.redisson.api.RedissonClient;
+import org.redisson.api.*;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
 
 import java.util.Iterator;
 
@@ -17,7 +19,7 @@ import java.util.Iterator;
 @SpringBootTest(classes = {RedissonApplication.class})
 public class RedissonApplicationTest {
 
-    @Autowired
+    /*@Autowired
     private Config config;
 
     @Test
@@ -29,6 +31,31 @@ public class RedissonApplicationTest {
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
+
+    }*/
+
+    @Autowired
+    private RedissonClient client;
+
+    @Autowired
+    @Qualifier("rc")
+    private RedissonReactiveClient reactiveClient;
+
+    @Test
+    public void syncTest(){
+        RAtomicLong myLong = client.getAtomicLong("mylong");
+        boolean b = myLong.compareAndSet(222, 401);
+    }
+
+    @Test
+    public void  asyncTest(){
+        RAtomicLong mylong = client.getAtomicLong("mylong");
+        RFuture<Boolean> future = mylong.compareAndSetAsync(222, 4);
+        future.whenComplete((aBoolean, throwable) ->{
+            Assert.assertEquals(aBoolean,false);
+            System.out.println("==============");
+        });
+        RLiveObjectService liveObjectService = client.getLiveObjectService();
 
     }
 }
